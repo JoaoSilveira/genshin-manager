6a4bb2f68a68cb1430fc8f22230ece0ffcbef975
João Pedro <joao.pedro0842@hotmail.com>
2023-03-08 15:35:21 -0300

    Fix comparison error

--- src/BuildAccordion.svelte
+++ src/BuildAccordion.svelte
@@ -36,7 +36,7 @@
             };
         } else {
             const arr = $selected_build.char_build;
-            const id = arr.length >= 0 ? arr[arr.length - 1].id + 1 : 0;
+            const id = arr.length > 0 ? arr[arr.length - 1].id + 1 : 0;
             arr.push({ ...event.detail, id });
 
             $build_list[$build_index] = {
