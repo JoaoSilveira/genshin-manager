5f804fda26f65d248f8bd25c4ca70ef515d91487
João Pedro <joao.pedro0842@hotmail.com>
2023-05-25 11:04:31 -0300

    Initial svelte-kit commit

--- src/stores/genshinData.ts
+++
@@ -1,7 +0,0 @@
-import { readable } from "svelte/store";
-import genshinDataTransform from "../lib/genshinDataTransform";
-import pristine from '../../data_test/genshin_data.json';
-
-const genshinData = genshinDataTransform(pristine as unknown as GenshinDataPristine);
-
-export default genshinData;
